9a0d14f430c664bebe666f27ddc7f8d079c7e2ea
Daniel Keen <keendaniel01@gmail.com>
2018-03-26 16:29:31 +0100

    test: calculator subtract method works correctly :white_check_mark:

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -8,4 +8,10 @@ describe Calculator do
       expect(calculator.add([1, 4])).to eq 5
     end
   end
+
+  describe '#subtract' do
+    it 'takes two numbers and subtracts the last from the first' do
+      expect(calculator.subtract([4, 1])).to eq 3
+    end
+  end
 end

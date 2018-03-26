2f9e46743eedfee28b756212d10c57b2ec3e15ca
Daniel Keen <keendaniel01@gmail.com>
2018-03-26 14:54:55 +0100

    test: statement class inits with empty array of transactions

---
+++ spec/statement_spec.rb
@@ -0,0 +1,11 @@
+require 'statement'
+
+describe Statement do
+  subject(:statement) { described_class.new }
+
+  describe 'initialize' do
+    it 'initializes with an empty array of transactions' do
+      expect(statement.transactions).to eq []
+    end
+  end
+end

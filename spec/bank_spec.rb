7adac8ed44e3aed4e3ec0c77cdec0de5fddbba22
Daniel Keen <keendaniel01@gmail.com>
2018-03-26 11:06:02 +0100

    test: deposit increases bank balance:white_check_mark:

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -1,9 +1,18 @@
 require "bank"
 
 describe Bank do
-
   subject(:bank) { described_class.new }
-  it "initializes with a balance of 0" do
-    expect(bank.balance).to eq 0
+
+  describe 'initialize' do
+    it 'initializes with a balance of 0' do
+      expect(bank.balance).to eq 0
+    end
+  end
+
+  describe 'deposit' do
+    it 'increases balance by the amount being deposited' do
+      amount = 10
+      expect { bank.deposit(amount) }.to change { bank.balance }.by 10
+    end
   end
 end

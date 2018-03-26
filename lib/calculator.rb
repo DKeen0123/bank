1c8716e94fd02543eab0c0c73350761bc7154a2a
Daniel Keen <keendaniel01@gmail.com>
2018-03-26 16:27:41 +0100

    feat: calculator can add an array of numbers

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,3 +1,7 @@
 class Calculator
 
+  def add(numbers)
+    numbers.inject(:+)
+  end
+
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -5,7 +5,7 @@ describe Calculator do
 
   describe '#add' do
     it 'takes two numbers and adds them together' do
-      expect(calculator.add(1, 4)).to eq 5
+      expect(calculator.add([1, 4])).to eq 5
     end
   end
 end

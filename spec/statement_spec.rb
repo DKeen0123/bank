1b0cc8850d78dcabbc0d5384cdc50bc337bdad20
Daniel Keen <keendaniel01@gmail.com>
2018-03-26 15:01:10 +0100

    test: print_statement test re-written for statement class

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -8,4 +8,16 @@ describe Statement do
       expect(statement.transactions).to eq []
     end
   end
+
+  describe 'print_statement' do
+    date =  Time.now.strftime('%d/%m/%Y')
+    amount = 10
+    it 'prints a deposit statement in a user-friendly format' do
+      deposit = ['deposit', date, amount, amount]
+      statement.transactions.push(deposit)
+      expect { statement.print_statement(statement.transactions) }.to output(
+        "date: #{date}, credit: #{amount}, debit: 0, balance: #{amount}\n"
+      ).to_stdout
+    end
+  end
 end
